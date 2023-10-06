686ae1896d30dee27f0700c008f304d2f5540519
EdoWahdana <wahdanaedo@gmail.com>
2023-10-07 04:32:17 +0700

    feat: http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from 'utils/httpException';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(9092);
 }
 bootstrap();
---
+++ utils/httpException.ts
@@ -0,0 +1,39 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+interface HttpExceptionResponse {
+  statusCode: number;
+  message: any;
+  error: string;
+}
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const request = context.getRequest<Request>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? (exception.getResponse() as HttpExceptionResponse).message
+        : (exception as any).message;
+
+    response.status(status).json({
+      message,
+      statusCode: status,
+      path: request.url,
+    });
+  }
+}
